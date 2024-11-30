db4fcc063c77a3c11861537c155f010b86a53019
ilovegithub15 <142916985+ilovegithub15@users.noreply.github.com>
2024-11-30 16:31:38 -0600

    worked on function, checks input

--- stringextract.cpp
+++ stringextract.cpp
@@ -6,17 +6,34 @@ using namespace std;
 double extractNumeric(const string&str) {
     bool isValidNumber = true;
     bool isNegative = false;
+    bool hasDecimal = false;
     //Checks if string is positive
     if (str[0] == '-') {
             isNegative = true;
         }
     //Long while loop to ensure string is valid.
-    while (i <= str.length()); {
-        //Checks if string is positive
+    int i = 0;
+    while (i < str.length()) {
         //Ensures string is numbers only
-        if (str[i] != "0" || ) {
-            
+        if (str[i] != '0' || str[i] != '1' || str[i] != '2' || str[i] != '3' ||
+        str[i] != '4' || str[i] != '5' || str[i] != '6' || str[i] != '7'  || 
+        str[i] != '8' || str[i] != '9' || str[i] != '+' || str[i] != '-' || str[i] != '.') {
+            isValidNumber = false;
         }
+        //Checks if there is a + or - in the wrong position
+        if((str[i] != '+' || str[i] != '-') && (i != 0)) {
+            isValidNumber = false;
+        }
+        //Checks if the number has a decimal in it
+        if(str[i] == '.') {
+            hasDecimal = true;
+        }
+    }
+    if(isValidNumber == true) {
+        
+    } else {
+        //If answer is not valid, return this number.
+        return -999999.99;
     }
 
 }
